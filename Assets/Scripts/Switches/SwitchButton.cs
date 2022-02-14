f912763a17be22b8d00ab741daa5d7d43988aac1
Marta <martikajover@icloud.com>
2022-02-14 11:30:42 +0100

    Interruptores Codigo

---
+++ Assets/Scripts/Switches/SwitchButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchButton : SwitchesPax
+{
+    public override void SwitchInteract()
+    {
+        
+    }
+}
